cade36431c62f8a6710e75bdb318eef97a5c8968
wutong <wt01278@163.com>
2020-11-27 13:27:20 +0800

    'x'

--- source/js/FunnyTitle.js
+++ source/js/FunnyTitle.js
@@ -6,8 +6,7 @@ document.addEventListener('visibilitychange', function () {
         // $('[rel="icon"]').attr('href', "/funny.ico");
         document.title = '╭(°A°`)╮ 页面崩溃啦 ~';
         clearTimeout(titleTime);
-    }
-    else {
+    } else {
         // $('[rel="icon"]').attr('href', "/favicon.ico");
         document.title = '(ฅ>ω<*ฅ) 噫又好啦 ~' + OriginTitle;
         titleTime = setTimeout(function () {
